b3fe5ef0631daf3761a5a705a9204467a0fb52df
Samuel Osinloye <psalmueloye@gmail.com>
2020-12-12 16:29:08 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FastLearn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
